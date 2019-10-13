6ce076cb732d79cc7f64126611b30c576a3969a6
Celestine Ekoh-Ordan <CEO.ehis@outlook.com>
2019-10-13 11:19:57 +0100

    Updates

---
+++ precache-manifest.d5538fb33d1af17d69af4c8cf2c51f2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4fcfed0713e4971f0b3ee36f987e7f0",
+    "url": "/jaunt/index.html"
+  },
+  {
+    "revision": "a77a7ecda05eaeca20dc",
+    "url": "/jaunt/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4f09373aa35e8b360b84",
+    "url": "/jaunt/static/js/2.aedae2d5.chunk.js"
+  },
+  {
+    "revision": "a77a7ecda05eaeca20dc",
+    "url": "/jaunt/static/js/main.1c65285f.chunk.js"
+  },
+  {
+    "revision": "bfb137df18e56f9e8e30",
+    "url": "/jaunt/static/js/runtime-main.16757a82.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/jaunt/static/media/logo.25bf045c.svg"
+  }
+]);
